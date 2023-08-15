ff4d8daf3b09d513c58d893fb129626bc980f38f
AzorAha1 <wizfaiz@icloud.com>
2023-08-15 04:15:05 +0100

    used strtok to tokenize our input and split into arguments based on the delimiters given

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 int main()
 {
 	char *lineptr = NULL;
 	size_t buffersize = 0;
 	int line;
+	char *av[1024];
+	char *string_token;
+	int count;
+	pid_t execute;
 
 	for(;;)
 	{
@@ -13,5 +19,17 @@ int main()
 		{
 			perror("Error");
 		}
+		string_token = strtok(lineptr, " \n");
+		for (count = 0; string_token != NULL; count++)
+		{
+			av[count] = string_token;
+			string_token = strtok(NULL, " \n");
+		}
+		av[count] = NULL;
+		execute = execve(av[0], av, NULL);
+		if (execute == -1)
+		{
+			perror("Error");
+		}	
 	}
 }
