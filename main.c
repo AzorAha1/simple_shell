d5f75ea618fe32571fa6d126f7ab8b7ff80c3407
AzorAha1 <wizfaiz@icloud.com>
2023-08-15 20:02:29 +0100

    created a new process and waited for the parent process to finish in an attempt to keep the loop running even after execution

--- main.c
+++ main.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
 int main()
 {
 	char *lineptr = NULL;
 	size_t buffersize = 0;
 	int line;
-	char *av[1024];
+	char *av[200];
 	char *string_token;
 	int count;
-	pid_t execute;
+	pid_t c_process;
 
 	for(;;)
 	{
@@ -18,6 +21,7 @@ int main()
 		if (line == -1)
 		{
 			perror("Error");
+			return (-1);
 		}
 		string_token = strtok(lineptr, " \n");
 		for (count = 0; string_token != NULL; count++)
@@ -26,10 +30,26 @@ int main()
 			string_token = strtok(NULL, " \n");
 		}
 		av[count] = NULL;
-		execute = execve(av[0], av, NULL);
-		if (execute == -1)
+		c_process = fork();
+		if (c_process == 0)
 		{
-			perror("Error");
-		}	
+			int execute = execve(av[0], av, NULL);
+			if ((execute = 0))
+			{
+				write(1, lineptr, sizeof(lineptr));
+				return (0);
+			}
+			if ((execute == -1))
+			{
+				perror("Error");
+				return (-1);
+			}
+		}
+		else if ((c_process != 0))
+		{
+			wait(NULL);
+		}
 	}
+	free(lineptr);
+	return (1);
 }
