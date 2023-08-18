5828b74a04e75c54cecf41673979338690b81037
AzorAha1 <wizfaiz@icloud.com>
2023-08-18 23:01:55 +0100

    changes in string compare

--- _strcmp.c
+++ _strcmp.c
@@ -7,16 +7,19 @@
  * Description - strcmp
  * Return: this this the return type
  */
-int _strcmp(char *s1, char *s2)
+int _strcmp(const char *s1, const char *s2)
 {
 	int i;
+	
+	if (!s1 || !s2)
+		return (-1);
 
-	for (i = 0; s1[i] != '\0' && s2[1] != '\0'; i++)
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
 		if (s1[i] != s2[i])
 		{
 			return (s1[i] - s2[i]);
 		}
 	}
-	return (s1[i] - s2[i]);
+	return (0);
 }
